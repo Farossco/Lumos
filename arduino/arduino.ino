eea030d58af7aee0420ae6a5ece1f3369b7c972f
Farès Chati <faresticha@gmail.com>
2017-01-19 20:43:15 +0100

    Arduino code changes
    - Debug mode
    - Some line commenting
    - Added serial communication with ESP8266

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,6 +4,8 @@
 #define LEDBLUE 10 // Blue LED OUT pin
 #define INFRARED 11 // Infrared IN pin
 
+#define DEBUG false // DEBUG Mode
+
 #define MINPOWER 5 // Minimum power value
 #define MAXPOWER 100 // Maximum power value
 #define POWERSPEED 5 // Power increasion or dicreasion speed
@@ -12,6 +14,9 @@
 #define MINSTROBE 2 // Minimum Strobe speed
 #define MAXSTROBE 50 // Maximum Strobe speed
 #define STROBESPEED 1 // Strobe increasion or dicreasion speed
+
+#define TYPERGB 3
+#define TYPEON 2
 
 // Infrared reception objects declaration
 IRrecv irrecv(INFRARED);
@@ -23,10 +28,10 @@ long color[][3] =
 { {0xFFFFFF, 0xFFA857, 0xA3C8EDDB}, //WHITE
 
   {0xFF0000, 0xFF9867, 0x97483BFB}, //R1
-  {0xFF712C, 0xFFE817, 0x5BE75E7F}, //R2
-  {0xFF5E2D, 0xFF02FD, 0xD7E84B1B}, //R3
-  {0xFFC506, 0xFF50AF, 0x2A89195F}, //R4
-  {0xFFF700, 0xFF38C7, 0x488F3CBB}, //R5
+  {0xFF5300, 0xFFE817, 0x5BE75E7F}, //R2
+  {0xFF3C00, 0xFF02FD, 0xD7E84B1B}, //R3
+  {0xFFC400, 0xFF50AF, 0x2A89195F}, //R4
+  {0xFFFF00, 0xFF38C7, 0x488F3CBB}, //R5
 
   {0x00FF00, 0xFFD827, 0x86B0E697}, //G1
   {0x20FF5D, 0xFF48B7, 0xF377C5B7}, //G2
@@ -46,7 +51,7 @@ int lastMode; // Mode in previous loop - Allows mode initializations
 int mode; // Current lighting mode (0: Constant lightning / 1: Flash / 2: Strobe / 3: Fade / 4: Smooth)
 int count; // Strobe delay counting
 int strobeSpeed; // Current Strobe speed (From MINSTROBE to MAXSTROBE)
-long RGB; // Currently displayed RGB value (From 0x000000 to 0xFFFFFF)
+unsigned long RGB; // Currently displayed RGB value (From 0x000000 to 0xFFFFFF)
 int RED; // Currentlty red value including lightning power (From 0 to 255)
 int unpoweredRED; // Currentlty red value without lightning power (From 0 to 255)
 int GREEN; // Currentlty green value including lightning power (From 0 to 255)
@@ -58,6 +63,8 @@ int i; // Counting variable (used for "for" statements)
 float power; // Current lightning power (from MINPOWER to MAXPOWER)
 long lastIRCode; // IR code in previous loop - Allows continious power / strobe speed increasion / dicreasion
 long IRCode; // Current IR code
+byte buff[20];
+int wordLength;
 
 void setup()
 {
@@ -67,12 +74,12 @@ void setup()
   pinMode(LEDBLUE, OUTPUT);
   pinMode(INFRARED, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(250000); // Initialize serial communication
+  Serial.begin(115200); // Initialize serial communication
   on = false; // LEDs are off on startup
   irrecv.enableIRIn(); // Initialize IR communication
   RGB = 0xFFFFFF; // Initialize color to white
   mode = 0; // Initialize mode to constant lightning
-  power = 100; // Initializing power to 10
+  power = 30; // Initializing power to 10
   // Initializing all other variables to 0
   lastMode = 0;
   state = 0;
@@ -85,6 +92,8 @@ void setup()
 void loop()
 {
   readInfrared(); // Read the in-comming IR signal if present
+
+  readSerial();
 
   action(); // Do something according to current mode
 
@@ -144,107 +153,109 @@ void readInfrared()
     if (IRCode == 0xFFFFFFFF)
       IRCode = lastIRCode;
 
-    switch (IRCode)
+    if (IRCode == 0xFFB04F || IRCode == 0xF0C41643)
     {
-      // ON
-      case 0xFFB04F:
-      case 0xF0C41643:
-        mode = 0;
-        lastMode = 0;
-        on = true;
-        lastIRCode = 0;
-        break;
-        break;
+      on = true;
+      lastIRCode = 0;
+    }
 
-      //OFF
-      case 0xFFF807:
-      case 0xE721C0DB:
-        mode = 0;
-        lastMode = 0;
-        on = false;
-        lastIRCode = 0;
-        break;
-        break;
+    if (on)
+    {
+      switch (IRCode)
+      {
+        //OFF
+        case 0xFFF807:
+        case 0xE721C0DB:
+          on = false;
+          lastIRCode = 0;
+          break;
+          break;
 
-      //DOWN
-      case 0xFFB847:
-      case 0xA23C94BF:
-        if (mode == 2)
-        {
-          if (strobeSpeed - STROBESPEED >= MINSTROBE) strobeSpeed -= STROBESPEED;
-          else strobeSpeed = MINSTROBE;
-        }
-        else
-        {
-          if (power - POWERSPEED >= MINPOWER) power -= POWERSPEED;
-          else power = MINPOWER;
-        }
+        //DOWN
+        case 0xFFB847:
+        case 0xA23C94BF:
+          if (mode == 2)
+          {
+            if (strobeSpeed - STROBESPEED >= MINSTROBE) strobeSpeed -= STROBESPEED;
+            else strobeSpeed = MINSTROBE;
+          }
+          else
+          {
+            if (power - POWERSPEED >= MINPOWER) power -= POWERSPEED;
+            else power = MINPOWER;
+          }
 
-        rgb2color();
-        lastIRCode = IRCode;
-        Serial.print("Power: ");
-        Serial.println(power);
-        Serial.print("RED: ");
-        Serial.print(RED);
-        Serial.print(" / GREEN: ");
-        Serial.print(GREEN);
-        Serial.print(" / BLUE: ");
-        Serial.println(BLUE);
-        break;
-        break;
+          rgb2color();
+          lastIRCode = IRCode;
+          if (DEBUG)
+          {
+            Serial.print("Power: ");
+            Serial.println(power);
+            Serial.print("RED: ");
+            Serial.print(RED);
+            Serial.print(" / GREEN: ");
+            Serial.print(GREEN);
+            Serial.print(" / BLUE: ");
+            Serial.println(BLUE);
+          }
+          break;
+          break;
 
-      //UP
-      case 0xFF906F:
-      case 0xE5CFBD7F:
-        if (mode == 2)
-        {
-          if (strobeSpeed + STROBESPEED <= MAXSTROBE) strobeSpeed += STROBESPEED;
-          else strobeSpeed = MAXSTROBE;
-        }
-        else
-        {
-          if (power + POWERSPEED <= MAXPOWER) power += POWERSPEED;
-          else power = MAXPOWER;
-        }
-        rgb2color();
-        lastIRCode = IRCode;
-        Serial.print("Power: ");
-        Serial.println(power);
-        Serial.print("RED: ");
-        Serial.print(RED);
-        Serial.print(" / GREEN: ");
-        Serial.print(GREEN);
-        Serial.print(" / BLUE: ");
-        Serial.println(BLUE);
-        break;
-        break;
+        //UP
+        case 0xFF906F:
+        case 0xE5CFBD7F:
+          if (mode == 2)
+          {
+            if (strobeSpeed + STROBESPEED <= MAXSTROBE) strobeSpeed += STROBESPEED;
+            else strobeSpeed = MAXSTROBE;
+          }
+          else
+          {
+            if (power + POWERSPEED <= MAXPOWER) power += POWERSPEED;
+            else power = MAXPOWER;
+          }
+          rgb2color();
+          lastIRCode = IRCode;
+          if (DEBUG)
+          {
+            Serial.print("Power: ");
+            Serial.println(power);
+            Serial.print("RED: ");
+            Serial.print(RED);
+            Serial.print(" / GREEN: ");
+            Serial.print(GREEN);
+            Serial.print(" / BLUE: ");
+            Serial.println(BLUE);
+          }
 
-      //STROBE
-      case 0xFF00FF:
-      case 0xFA3F159F:
-        mode = 2;
-        break;
-        break;
+          break;
+          break;
 
-      //FADE
-      case 0xFF58A7:
-      case 0xDC0197DB:
-        mode = 3;
-        break;
-        break;
+        //STROBE
+        case 0xFF00FF:
+        case 0xFA3F159F:
+          mode = 2;
+          break;
+          break;
+
+        //FADE
+        case 0xFF58A7:
+        case 0xDC0197DB:
+          mode = 3;
+          break;
+          break;
 
 
-      //SMOOTH
-      case 0xFF30CF:
-      case 0x9716BE3F:
-        mode = 4;
-        break;
-        break;
+        //SMOOTH
+        case 0xFF30CF:
+        case 0x9716BE3F:
+          mode = 4;
+          break;
+          break;
 
-      //COLORS
-      default:
-        lastIRCode = 0;
-        if (on)
+        //COLORS
+        default:
+          lastIRCode = 0;
           for (i = 0; i < N; i++)
             if (results.value == color[i][1] || results.value == color[i][2])
             {
@@ -252,13 +263,116 @@ void readInfrared()
               lastMode = 0;
               RGB = color[i][0];
             }
-        break;
+          break;
+      }
     }
-    Serial.println(IRCode, HEX);
+    if (DEBUG)
+    {
+      Serial.print("Incomming IR: ");
+      Serial.println(IRCode, HEX);
+    }
+
     irrecv.resume();
   }
 }
 
+void readSerial()
+{
+  if (!Serial.available()) return;
+
+  boolean flag;
+  int infoType, i, n;
+  char charArray[20];
+  char charRgb[7];
+  String str;
+
+  for (n = 0; Serial.available() && n < 20; n++)
+  {
+    charArray[n] = Serial.read();
+    //delay(1);
+  }
+
+  for (i = 0; i < n; i++)
+    str += String(charArray[i]);
+  /*
+    flag = false;
+
+      for (i = 0; i < str.length(); i++)
+        if (str.charAt(i) < '0' || str.charAt(i) > '9' && str.charAt(i) < 'A' || str.charAt(i) > 'Z' && str.charAt(i) < 'a' || str.charAt(i) > 'z')
+          return;
+
+      for (i = 0; i < str.length(); i++)
+        if (str.charAt(i) >= '0' && str.charAt(i) <= '9' || str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+          flag = true;
+
+      if (!flag) return;
+  */
+
+  if (str.indexOf("ON") != -1)
+    infoType = TYPEON;
+  else if (str.indexOf("RGB") != -1)
+    infoType = TYPERGB;
+  else
+    return;
+
+  if (infoType == TYPEON && str.length() != 3)
+    return;
+  if (infoType == TYPERGB && (str.length() < 3 || str.length() > 9))
+    return;
+
+  if (DEBUG)
+  {
+    Serial.print("Word: ");
+    Serial.println(str);
+    Serial.print("Length: ");
+    Serial.println(str.length());
+    Serial.print("Type: ");
+    Serial.println(infoType == TYPEON ? "ON" : "RGB");
+  }
+
+  str.remove(0, infoType); // Remove 2 first characters if "ON" type and 3 first ones if "RGB" type
+
+  if (DEBUG)
+  {
+    Serial.print(infoType == TYPEON ? "ON: " : "RGB: ");
+    Serial.println(infoType == TYPERGB ? str : str == "1" ? "True" : str == "0" ? "False" : "Error");
+    if (infoType == TYPEON)
+      Serial.println();
+  }
+
+  if (infoType == TYPERGB)
+    while (str.length() < 6)
+      str = "0" + str;
+
+  if (DEBUG)
+  {
+    if (infoType == TYPERGB)
+    {
+      Serial.print("Full RGB: ");
+      Serial.println(str);
+    }
+  }
+
+  if  (infoType == TYPEON)
+  {
+    on = str == "1" ? true : false;
+  }
+  else
+  {
+    str.toCharArray(charRgb, 7);
+    RGB = strtol(charRgb, NULL, 16);
+  }
+
+  if (DEBUG)
+  {
+    if (infoType == TYPERGB)
+    {
+      Serial.print("Full RGB (number): ");
+      Serial.println(RGB, HEX);
+      Serial.println();
+    }
+  }
+}
 
 void action()
 {
@@ -294,7 +408,6 @@ void initStrobe()
   state = 0;
   RGB = 0xFFFFFF;
   rgb2color();
-  power = 0;
   lastMode = 2;
   count = 0;
 }
@@ -313,9 +426,9 @@ void STROBE()
 
 
   if (state)
-    power = MAXPOWER;
+    RGB = 0xFFFFFF;
   else
-    power = 0;
+    RGB = 0x000000;
 }
 
 
