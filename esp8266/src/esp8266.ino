67e9a387290c226f03149dbdda12bac8ef332fc5
Farès Chati <faresticha@gmail.com>
2017-02-15 17:53:24 +0100

    [ESP8266] Added prayer time downloading
    + Changed time downloading method

--- esp8266/src/esp8266.ino
+++ esp8266/src/esp8266.ino
@@ -1,41 +1,54 @@
-#include <TimeLib.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <TimeLib.h>
 #include <WiFiUdp.h>
 
-#define BAUD_RATE 250000 // Serial baud rate
-
-#define DEBUG_ENABLED false
-
-//Some IDs used for serial reception decrypt
-#define TYPE_ERROR -1
-#define TYPE_RGB 1
-#define TYPE_ON 2
-#define TYPE_POW 3
-#define TYPE_MOD 4
-
-#define TIME_ZONE 1 // Central European Time
-
-#define LOCAL_PORT 8888 // Local port to listen for UDP packets
+#define BAUD_RATE     250000 // Serial baud rate
+#define DEBUG_ENABLED true   // Debug mode
 
 // Wi-Fi informations
 #define SSID0 "Patatou"
 #define PASS0 "FD00000000"
 
-WiFiUDP Udp;
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-IPAddress timeServer (132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
+// Some IDs used for serial reception decrypt
+#define TYPE_ERROR -1
+#define TYPE_RGB   1
+#define TYPE_ON    2
+#define TYPE_POW   3
+#define TYPE_MOD   4
 
+// Prayer times
+#define REQUEST_TIMEOUT 5000
+#define HTTP_PORT       80
+#define HOST            "api.aladhan.com"
+#define LATITUDE        "48.866667"
+#define LONGITUDE       "2.333333"
+#define TIME_ZONE       "Europe/Paris"
+#define METHOD          1
+
+// Wifi webserver
 WiFiServer server (80);
 WiFiClient client;
 
+// Web server requests
 int infoType;
 String request;
-
-// Variables for web request reading
 byte buf[20];
 long value;
 boolean error;
+
+// Serial requests
+int n;
+char messageChar[20];
+String message;
+
+// Prayer time request
+DynamicJsonBuffer jsonBuffer;
+String line;
+unsigned long timeout;
+int code, prayerTime[6][3];
+const char * json, * status, * timestamp, * prayerTimeString[6];
+const char * prayersName[] = { "Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha" };
 
 void setup ()
 {
@@ -50,20 +63,21 @@ void setup ()
 	// Connect to WiFi network
 	if (DEBUG_ENABLED)
 	{
-		Serial.println ();
-		Serial.println ();
+		Serial.println();
+		Serial.println();
 		Serial.print ("Connecting to ");
 		Serial.println (SSID0);
 	}
 
 	WiFi.begin (SSID0, PASS0);
 
-	while (WiFi.status () != WL_CONNECTED)
+	while (WiFi.status() != WL_CONNECTED)
 	{
 		delay (500);
-		if (DEBUG_ENABLED)
-			Serial.print (".");
+
+		if (DEBUG_ENABLED) Serial.print (".");
 	}
+
 	if (DEBUG_ENABLED)
 	{
 		Serial.println ("");
@@ -71,154 +85,148 @@ void setup ()
 	}
 
 	// Start the server
-	server.begin ();
+	server.begin();
 
 	if (DEBUG_ENABLED)
-	{
 		Serial.println ("Server started");
-
-		// Print the IP address
-		Serial.print ("Use this URL to connect: ");
-		Serial.print ("http://");
-		Serial.print (WiFi.localIP ());
-		Serial.println ("/");
-	}
-
-	sendTime ();
-}
+} // setup
 
 void loop ()
 {
-	readSerial ();
+	readSerial();
 
-	// Check if a client has connected
-	client = server.available ();
-
-	if (!client)
-		return;
-
-	// Wait until the client sends some data
-	while (!client.available ())
-		delay (1);
-
-	// Read the first line of the request
-	request = client.readStringUntil ('\r');
-
-	if (DEBUG_ENABLED)
-	{
-		Serial.println ();
-		Serial.println (request);
-	}
-
-	client.flush ();
-
-	// Match the request
-	if (request.indexOf ("/RGB=") != -1)
-	{
-		getRgb ();
-		infoType = TYPE_RGB;
-	}
-	else if (request.indexOf ("/ON=") != -1)
-	{
-		getOn ();
-		infoType = TYPE_ON;
-	}
-	else if (request.indexOf ("/POW=") != -1)
-	{
-		getPow ();
-		infoType = TYPE_POW;
-	}
-	else if (request.indexOf ("/MOD=") != -1)
-	{
-		getMode ();
-		infoType = TYPE_MOD;
-	}
-	else
-	{
-		value = -1;
-		infoType = TYPE_ERROR;
-	}
-
-	if (DEBUG_ENABLED)
-	{
-		if (value == -1)
-			Serial.println ("CODING ERROR !");
-	}
-
-	if (value != -1)
-	{
-		if (DEBUG_ENABLED)
-			Serial.print ("\nSending to Arduino: ");
-		Serial.print (
-				infoType == TYPE_RGB ? "RGB" : infoType == TYPE_ON ? "ON" :
-				infoType == TYPE_POW ? "POW" :
-				infoType == TYPE_MOD ? "MOD" : "");
-		Serial.print (value, infoType == 1 ? HEX : DEC);
-		if (DEBUG_ENABLED)
-			Serial.print ("\n");
-	}
-
-	// Return the response
-	client.print ("HTTP/1.1 ");
-	client.println (value == -1 ? "400 FORMAT ERROR" : "200 OK");
-	client.println ("Content-Type: text/html");
-	client.println (""); // Do not forget this one
-	if (value == -1)
-		client.println ("Format error !");
-	else
-		client.println (value, infoType == TYPE_RGB ? HEX : DEC);
-}
+	readWeb();
+} // loop
 
 void readSerial ()
 {
-	if (!Serial.available ())
+	// If nothing is comming, we stop here
+	if (!Serial.available())
 		return;
 
-	int n;
-	char messageChar[20];
-	String message;
+	message = "";
 
-	for (n = 0; Serial.available () && n < 20; n++)
+	for (n = 0; n < 20 && Serial.available(); n++)
 	{
-		messageChar[n] = Serial.read ();
+		messageChar[n] = Serial.read();
 		delay (1);
 	}
 
+	// Converting char array into String
 	for (int i = 0; i < n; i++)
 		message += String (messageChar[i]);
 
+	// If the Arduino is asking for time
 	if (message.indexOf ("TIMEPLEASE") != -1)
 	{
 		if (DEBUG_ENABLED)
 			Serial.println ("\nReceived time request from arduino");
 
-		sendTime ();
+		// We send the time to the Arduino
+		sendTime();
 	}
 }
 
-void sendTime ()
+void readWeb ()
 {
-	if (DEBUG_ENABLED)
-		Serial.println ("\nStarting UDP");
+	// Check if a client has connected
+	client = server.available();
 
-	Udp.begin (LOCAL_PORT);
+	// If nobody connected, we stop here
+	if (!client) return;
 
+	// Wait until the client sends some data
+	while (!client.available())
+		delay (1);
+
+	// Read the first line of the request
+	request = client.readStringUntil ('\r');
+
+	// [DEBUG] Printing the incomming request
 	if (DEBUG_ENABLED)
 	{
-		Serial.print ("Local port: ");
-		Serial.println (Udp.localPort ());
-		Serial.println ("waiting for sync");
+		Serial.println();
+		Serial.println (request);
 	}
 
-	setSyncProvider (getNtpTime);
+	client.flush();
+
+	// Match the request
+	if (request.indexOf ("/RGB=") != -1)
+	{
+		getRgb();
+		infoType = TYPE_RGB;
+	}
+	else if (request.indexOf ("/ON=") != -1)
+	{
+		getOn();
+		infoType = TYPE_ON;
+	}
+	else if (request.indexOf ("/POW=") != -1)
+	{
+		getPow();
+		infoType = TYPE_POW;
+	}
+	else if (request.indexOf ("/MOD=") != -1)
+	{
+		getMode();
+		infoType = TYPE_MOD;
+	}
+	else
+	{
+		value    = -1;
+		infoType = TYPE_ERROR;
+	}
+
+	if (value == -1)
+	{
+		if (DEBUG_ENABLED)
+			Serial.println ("CODING ERROR !");
+	}
+	else
+	{
+		if (DEBUG_ENABLED)
+			Serial.print ("\nSending to Arduino: ");
+
+		// Print suffixe according to the type
+		Serial.print
+		(
+			infoType == TYPE_RGB ? "RGB" :
+			infoType == TYPE_ON ? "ON" :
+			infoType == TYPE_POW ? "POW" :
+			infoType == TYPE_MOD ? "MOD" : ""
+		);
+
+		// Now printing the correct vale in hexadecimal for RGB type and in decimal for any other type
+		Serial.print (value, infoType == TYPE_RGB ? HEX : DEC);
+
+		if (DEBUG_ENABLED)
+			Serial.println();
+	}
+
+	// Return the response to the client
+	client.print ("HTTP/1.1 ");
+	client.println (value == -1 ? "400 FORMAT ERROR" : "200 OK");
+	client.println ("Content-Type: text/html");
+	client.println (""); // Do not forget this one
+
+	if (value == -1)
+		client.println ("Format error !");
+	else
+		client.println (value, infoType == TYPE_RGB ? HEX : DEC);
+} // readWeb
+
+void sendTime ()
+{
+	getPrayerTime();
 
 	if (DEBUG_ENABLED)
 	{
 		Serial.print ("\nTime is: ");
-		digitalClockDisplay ();
+		digitalClockDisplay();
 	}
 
-	if (timeStatus () != timeNotSet)
+	if (timeStatus() != timeNotSet)
 	{
 		if (DEBUG_ENABLED)
 		{
@@ -227,15 +235,33 @@ void sendTime ()
 		}
 
 		Serial.print ("TIM");
-		Serial.print (now ());
+		Serial.print (now());
+
+		for (int i = 0; i < 6; i++)
+		{
+			if (DEBUG_ENABLED)
+			{
+				Serial.print ("\nSending prayer time to arduino (");
+				Serial.print (prayersName[i]);
+				Serial.print (") : ");
+				delay (1);
+			}
+			Serial.print ("PRT");
+			Serial.print (prayersName[i][0]); // First letter of the prayer name
+			Serial.print (prayerTime[i][2]);
+			delay (1);
+		}
 	}
 	else if (DEBUG_ENABLED)
+	{
 		Serial.print ("\nTime not set, not sending time to arduino...");
+	}
 
 	if (DEBUG_ENABLED)
-		Serial.println ();
-}
+		Serial.println();
+} // sendTime
 
+// Decoding RGB request
 void getRgb ()
 {
 	value = 0;
@@ -243,6 +269,7 @@ void getRgb ()
 
 	request.getBytes (buf, 16);
 
+	// [DEBUG] Printing the incomming value ascii code
 	if (DEBUG_ENABLED)
 	{
 		for (int i = 9; i < 15; i++)
@@ -253,17 +280,23 @@ void getRgb ()
 		Serial.println ("\n ----------- ");
 	}
 
+	// Converting values from their ascii
 	for (int i = 9; i < 15; i++)
 	{
-		if (buf[i] >= '0' && buf[i] <= '9')
+		if ((buf[i] >= '0') && (buf[i] <= '9'))
+		{
 			buf[i] = buf[i] - '0';
-		else if (buf[i] >= 'A' && buf[i] <= 'F')
+		}
+		else if ((buf[i] >= 'A') && (buf[i] <= 'F'))
+		{
 			buf[i] = buf[i] - 'A' + 10;
-		else
+		}
+		else // If one of the byte is not an hexadecimal value
 		{
 			error = true;
 			break;
 		}
+		// [DEBUG] Now printing decoded values
 		if (DEBUG_ENABLED)
 		{
 			Serial.print (buf[i], HEX);
@@ -271,39 +304,45 @@ void getRgb ()
 		}
 	}
 
+	// Converting it to an integer
 	for (int i = 9; i < 15; i++)
-	{
 		value += buf[i] * pow (16, 5 - (i - 9));
-	}
 
+	// [DEBUG] Printing the converted value
 	if (DEBUG_ENABLED)
 	{
-		Serial.println ();
+		Serial.println();
 		Serial.print ("\nRGB = ");
 		Serial.println (value, HEX);
 	}
 
+	// If an error occured during the decoding, set the value to -1
 	if (error)
 		value = -1;
-}
+} // getRgb
 
+// Decoding on/off request
 void getOn ()
 {
 	request.getBytes (buf, 10);
 
+	// Only reading byte at position 8 and converting it to an integer
 	value = buf[8] - '0';
 
+	// [DEBUG] Printing that value
 	if (DEBUG_ENABLED)
 	{
-		Serial.println ();
+		Serial.println();
 		Serial.print ("ON = ");
 		Serial.println (value);
 	}
 
-	if (value != 0 && value != 1)
+	// if the value is not correct, set it to -1
+	if ((value != 0) && (value != 1))
 		value = -1;
 }
 
+// Decoding power request
 void getPow ()
 {
 	value = 0;
@@ -311,6 +350,7 @@ void getPow ()
 
 	request.getBytes (buf, 13);
 
+	// [DEBUG] Printing the incomming value ascii code
 	if (DEBUG_ENABLED)
 	{
 		for (int i = 9; i < 12; i++)
@@ -321,15 +361,19 @@ void getPow ()
 		Serial.println ("\n ----------- ");
 	}
 
+	// Converting values from their ascii
 	for (int i = 9; i < 12; i++)
 	{
-		if (buf[i] >= '0' && buf[i] <= '9')
+		if ((buf[i] >= '0') && (buf[i] <= '9'))
+		{
 			buf[i] = buf[i] - '0';
-		else
+		}
+		else // If one of the byte is not an decimal value
 		{
 			error = true;
 			break;
 		}
+
 		if (DEBUG_ENABLED)
 		{
 			Serial.print (buf[i], DEC);
@@ -337,22 +381,24 @@ void getPow ()
 		}
 	}
 
+	// Converting it to an integer
 	for (int i = 9; i < 12; i++)
-	{
 		value += buf[i] * pow (10, 2 - (i - 9));
-	}
 
+	// [DEBUG] Printing the converted value
 	if (DEBUG_ENABLED)
 	{
-		Serial.println ();
+		Serial.println();
 		Serial.print ("\nPOW = ");
 		Serial.println (value);
 	}
 
-	if (value < 0 || value > 100 || error)
+	// If an error occured or the value is not between 0 and 100, set it to -1
+	if ((value < 0) || (value > 100) || error)
 		value = -1;
-}
+} // getPow
 
+// Decoding mode request
 void getMode ()
 {
 	request.getBytes (buf, 11);
@@ -361,116 +407,213 @@ void getMode ()
 
 	if (DEBUG_ENABLED)
 	{
-		Serial.println ();
+		Serial.println();
 		Serial.print ("Mode = ");
 		Serial.println (value);
 		Serial.print ("Mode (Text) = ");
-		Serial.println (
-				value == 0 ? "DEFAULT" : value == 1 ? "FLASH" :
-				value == 2 ? "STROBE" : value == 3 ? "FADE" :
-						value == 4 ?
-										"SMOOTH" :
-						value == 5 ?
-										"WAKEUP" : "UNKNOWN");
+		Serial.println (value == 0 ?
+		  "DEFAULT" :
+		  value == 1 ?
+		  "FLASH" :
+		  value == 2 ?
+		  "STROBE" :
+		  value == 3 ?
+		  "FADE" :
+		  value == 4 ? "SMOOTH" :
+		  value == 5 ? "WAKEUP" :
+		  "UNKNOWN");
 	}
 
-	if (value < 0 || value > 5)
-		value = -1;
+	if ((value < 0) || (value > 5)) value = -1;
 }
 
+// Digital clock display of the time
 void digitalClockDisplay ()
 {
-	// digital clock display of the time
-	printDigits (day ());
+	printDigits (day());
 	Serial.print ("/");
-	printDigits (month ());
+	printDigits (month());
 	Serial.print ("/");
-	Serial.print (year ());
+	Serial.print (year());
 
 	Serial.print (" ");
 
-	printDigits (hour ());
+	printDigits (hour());
 	Serial.print (":");
-	printDigits (minute ());
+	printDigits (minute());
 	Serial.print (":");
-	printDigits (second ());
+	printDigits (second());
 
-	Serial.println ();
+	Serial.println();
 }
 
+// Utility for digital clock display: prints preceding colon and leading 0
 void printDigits (int digits)
 {
-	// utility for digital clock display: prints preceding colon and leading 0
 	if (digits < 10)
 		Serial.print ('0');
 	Serial.print (digits);
 }
 
-/*-------- NTP code ----------*/
-
-time_t getNtpTime ()
+void getPrayerTime ()
 {
-	int i = 1;
+	Serial.print ("\nConnecting to ");
+	Serial.println (HOST);
 
-	while (i < 16)
+	// Use WiFiClient class to create TCP connections
+
+	if (!client.connect (HOST, HTTP_PORT))
 	{
-		while (Udp.parsePacket () > 0)
-			; // discard any previously received packets
+		Serial.println ("\nConnection failed");
+		return;
+	}
 
-		if (DEBUG_ENABLED)
-		{
-			Serial.print ("Transmit NTP Request ");
-			Serial.print ("(Attempt ");
-			Serial.print (i);
-			Serial.println (")");
-		}
+	// We now create an url for the request
+	String url = "/timings/0?latitude=";
+	url += LATITUDE;
+	url += "&longitude=";
+	url += LONGITUDE;
+	url += "&timezonestring=";
+	url += TIME_ZONE;
+	url += "&method=";
+	url += METHOD;
 
-		sendNTPpacket (timeServer);
-		uint32_t beginWait = millis ();
-		while (millis () - beginWait < 1500)
+	// [DEBUG] Printing the url
+	if (DEBUG_ENABLED)
+	{
+		Serial.print ("\nRequesting URL: ");
+		Serial.println (url);
+		Serial.println();
+	}
+
+	// Sending web request
+	client.print (String ("GET ") + url + " HTTP/1.1\r\n" + "Host: " + HOST + "\r\n" + "Connection: close\r\n\r\n");
+	timeout = millis();
+
+	// Waiting for an answer
+	while (!client.available())
+	{
+		// If nothing come out after the timeout, we abort
+		if (millis() - timeout > REQUEST_TIMEOUT)
 		{
-			int size = Udp.parsePacket ();
-			if (size >= NTP_PACKET_SIZE)
-			{
-				if (DEBUG_ENABLED)
-					Serial.println ("\nReceive NTP Response !");
-				Udp.read (packetBuffer, NTP_PACKET_SIZE); // read packet into the buffer
-				unsigned long secsSince1900;
-				// convert four bytes starting at location 40 to a long integer
-				secsSince1900 = (unsigned long) packetBuffer[40] << 24;
-				secsSince1900 |= (unsigned long) packetBuffer[41] << 16;
-				secsSince1900 |= (unsigned long) packetBuffer[42] << 8;
-				secsSince1900 |= (unsigned long) packetBuffer[43];
-				return secsSince1900 - 2208988800UL + TIME_ZONE * SECS_PER_HOUR;
-			}
+			if (DEBUG_ENABLED)
+				Serial.println ("Client Timeout !");
+			client.stop();
+			return;
 		}
-		i++;
 	}
 
 	if (DEBUG_ENABLED)
-		Serial.println ("\nNo NTP Response :-(");
-	return 0; // return 0 if unable to get the time
-}
+	{
+		Serial.println ("Server answer:\n\n==================== Start ====================");
+	}
 
-// send an NTP request to the time server at the given address
-void sendNTPpacket (IPAddress &address)
-{
-	// set all bytes in the buffer to 0
-	memset (packetBuffer, 0, NTP_PACKET_SIZE);
-	// Initialize values needed to form NTP request
-	// (see URL above for details on the packets)
-	packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-	packetBuffer[1] = 0;     // Stratum, or type of clock
-	packetBuffer[2] = 6;     // Polling Interval
-	packetBuffer[3] = 0xEC;  // Peer Clock Precision
-	// 8 bytes of zero for Root Delay & Root Dispersion
-	packetBuffer[12] = 49;
-	packetBuffer[13] = 0x4E;
-	packetBuffer[14] = 49;
-	packetBuffer[15] = 52;
-	// all NTP fields have been given values, now
-	// you can send a packet requesting a timestamp:
-	Udp.beginPacket (address, 123); //NTP requests are to port 123
-	Udp.write (packetBuffer, NTP_PACKET_SIZE);
-	Udp.endPacket ();
-}
+	// Read all the lines of the reply from server and print them to Serial
+	while (client.available())
+	{
+		line = client.readStringUntil ('\r');
+		// [DEBUG] We print the line we're currently reading
+		if (DEBUG_ENABLED)
+			Serial.print (line);
+	}
+
+	if (DEBUG_ENABLED)
+	{
+		Serial.println ("\n===================== End =====================\n");
+	}
+
+	if (line.indexOf ("{\"code") == 1)
+	{
+		// The first charactere is a nl, so we don't want it
+		line = line.substring (1);
+		if (DEBUG_ENABLED)
+			Serial.println ("Success !");
+	}
+	else if (DEBUG_ENABLED)
+	{
+		Serial.print ("Failed ! (");
+		Serial.print (line.indexOf ("{\"code"));
+		Serial.println (")");
+	}
+
+	if (DEBUG_ENABLED)
+	{
+		Serial.println();
+		Serial.println ("Closing connection");
+	}
+
+	// At this point, the last line of the answer is in the line variable,
+	// that's actually the one we want
+
+	Serial.print ("\nJson: ");
+	Serial.println (line);
+
+	JsonObject& jsonRoot = jsonBuffer.parseObject (line);
+	code      = jsonRoot["code"];
+	status    = jsonRoot["status"];
+	timestamp = jsonRoot["data"]["date"]["timestamp"];
+
+	if (DEBUG_ENABLED)
+	{
+		Serial.print ("\nCode: ");
+		Serial.println (code);
+
+		Serial.print ("Status: ");
+		Serial.println (status);
+		Serial.println();
+	}
+
+	if (code != 200 && status != "OK")
+	{
+		if (DEBUG_ENABLED)
+			Serial.println ("Wrong status, leaving.");
+		return;
+	}
+
+	// Parsing all paryers time (Format HH:MM)
+	for (int i = 0; i < 6; i++)
+		prayerTimeString[i] = jsonRoot["data"]["timings"][prayersName[i]];
+
+	// Converting it into integer
+	for (int i = 0; i < 6; i++)
+	{
+		prayerTime[i][0] = (prayerTimeString[i][0] - '0') * 10 + prayerTimeString[i][1] - '0'; // Hour
+		prayerTime[i][1] = (prayerTimeString[i][3] - '0') * 10 + prayerTimeString[i][4] - '0'; // Minute
+		prayerTime[i][2] = prayerTime[i][0] * 60 + prayerTime[i][1];                           // Hour * 60 + minute
+	}
+
+	if (DEBUG_ENABLED)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			Serial.print (prayersName[i]);
+			Serial.print (": ");
+			Serial.println (prayerTimeString[i]);
+			Serial.print (prayersName[i]);
+			Serial.print (" (Integer): ");
+			Serial.print (prayerTime[i][0]);
+			Serial.print (":");
+			Serial.print (prayerTime[i][1]);
+			Serial.print (" (");
+			Serial.print (prayerTime[i][0] * 60 + prayerTime[i][1]);
+			Serial.print (" / ");
+			Serial.print (prayerTime[i][2]);
+			Serial.print (")");
+			Serial.println ("\n");
+		}
+
+		Serial.print ("Timestamp: ");
+		Serial.println (timestamp);
+	}
+
+	if (DEBUG_ENABLED)
+	{
+		Serial.print ("\nSetting Time to: ");
+		Serial.print (strtol (timestamp, NULL, 10) + 3600); // + 1h for the correct time in Paris
+		Serial.print (" (");
+		Serial.print (timestamp);
+		Serial.println (" + 3600)");
+	}
+
+	setTime (strtol (timestamp, NULL, 10) + 3600); // + 1h for the correct time in Paris
+} // getprayerTime
