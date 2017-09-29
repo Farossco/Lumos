edb34503359f3e1bcf04f4e97c53ffab31737175
Farès Chati <faresticha@gmail.com>
2017-09-29 21:08:36 +0200

    [Android] Changed ESP8266 IP address

--- android/app/src/main/java/fr/iclario/bedcontrol/Resources.java
+++ android/app/src/main/java/fr/iclario/bedcontrol/Resources.java
@@ -2,5 +2,5 @@ package fr.iclario.bedcontrol;
 
 public class Resources
 {
-	public static final String espAddress = "http://192.168.31.233";
+	public static final String espAddress = "http://192.168.31.173";
 }
