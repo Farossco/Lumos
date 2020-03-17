3d168f4dc9c620fe5b372e6740f241c834ee3768
Farès Chati <faresticha@gmail.com>
2020-03-17 14:08:15 +0100

    Made some constants private

--- src/Wifi.h
+++ src/Wifi.h
@@ -7,19 +7,10 @@
 
 # include <ESP8266WiFi.h>
 
-// Location
-# define LATITUDE    "48.866667"    // My latitude (Well... not really)
-# define LONGITUDE   "2.333333"     // My longitude (Not really either :p)
-# define TIME_ZONE   "Europe/Paris" // My time zone
-
 # define TIME_FORMAT "json"         // Format for receive the time
 # define TIME_KEY    "0D2WZ3KAP6GV" // TimeZoneDB API key
 # define TIME_BY     "zone"         // localization method
 # define TIME_FIELDS "timestamp"    // Needed fields in the answer
-
-// Wi-Fi informations
-# define SSID0 "Wificlariopc" // Wi-Fi SSID
-# define PASS0 "FC00000000"   // Wi-Fi password
 
 // Time
 const int TIME_REQUEST_TIMEOUT = 15000; // Request timeout
